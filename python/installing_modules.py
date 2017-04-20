e3634d95016c5e80acaedc5dd301a610c996e816
machau kumar <kumarma2212@gmail.com>
2017-04-20 22:59:48 +0530

    python

---
+++ python/installing_modules.py
@@ -0,0 +1,9 @@
+#using pip
+
+#pip install matplotlib
+#website
+#https://pypi.python.org/pypi
+
+#OR download pip file from
+#http://www.lfd.uci.edu/~gohlke/pythonlibs/
+#this is like .torrent file and use pip install this_filename
